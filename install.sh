c232f46cba4c106e565830813d02a831a1605426
Fw[a]rd <howard.main@outlook.com>
2022-03-21 00:11:01 +0800

    Fix bugs in install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 cp thu-checkin.py /usr/bin/thu-checkin.py
-cp thu-checkin.service thu-checkin.timer /etc/systemd/system
+cp thu-checkin@.service thu-checkin@.timer /etc/systemd/system
+
+chmod 755 /usr/bin/thu-checkin.py
 
 systemctl daemon-reload
